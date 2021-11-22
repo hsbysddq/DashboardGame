5bd74d21efbdfc068d14c1a59158cb3f3b14ad14
hsbysddq <hsby15sddq@gmail.com>
2021-11-22 08:03:35 +0700

    sa

--- routers/dashboards.router.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router()
-
-const { viewDashboard } = require('../controllers/dashboards.controller')
-const restrict = require('../middlewares/restrict.middleware')
-
-router.get('/dashboard', restrict, viewDashboard)
-
-module.exports = router
